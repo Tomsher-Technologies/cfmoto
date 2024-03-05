b46ceb8705e7f1464746acbbbb097aa79056de11
Dhanya-Tomsher <dhanya@tomsher.com>
2024-03-05 10:44:38 +0400

    push@4.47

--- resources/views/layouts/app/main.blade.php
+++ resources/views/layouts/app/main.blade.php
@@ -12,7 +12,7 @@
       <link rel="stylesheet" href="{{ asset('assets/css/jquery.hiSlider.css') }}">
       <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.css') }}">
       <link rel="stylesheet" href="{{ asset('assets/css/owl.theme.css') }}">
-      <link rel="stylesheet" href="{{ asset('assets/css/main.css?v1') }}">
+      <link rel="stylesheet" href="{{ asset('assets/css/main.css?v2') }}">
       <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
       <link rel="stylesheet" href="{{ asset('assets/css/s') }}">
       <linkrel="stylesheet"href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
